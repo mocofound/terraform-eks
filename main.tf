9316d020253910f83a2c1457e77dfc6c71069232
Alex Harness <9669770+mocofound@users.noreply.github.com>
2020-02-18 14:39:46 -0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,44 @@
+resource "aws_eks_cluster" "example" {
+  name     = "example"
+  role_arn = "${aws_iam_role.example.arn}"
+
+  vpc_config {
+    subnet_ids = ["${aws_subnet.example1.id}", "${aws_subnet.example2.id}"]
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    "aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy",
+    "aws_iam_role_policy_attachment.example-AmazonEKSServicePolicy",
+  ]
+}
+
+resource "aws_iam_role" "example" {
+  name = "eks-cluster-example"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = "${aws_iam_role.example.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = "${aws_iam_role.example.name}"
+}
