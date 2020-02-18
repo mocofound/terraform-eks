57399746dcd2ea4b4fc950e49abe3c0d2adb4676
Alex Harness <9669770+mocofound@users.noreply.github.com>
2020-02-18 14:47:42 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_iam_role_policy_attachment" "example-AmazonEKSServicePolicy" {
   role       = "${aws_iam_role.example.name}"
 }
 
-resource "aws_subnet" "main" {
+resource "aws_subnet" "example1" {
   vpc_id     = "${aws_vpc.main.id}"
   cidr_block = "10.0.1.0/24"
 
@@ -61,7 +61,7 @@ resource "aws_subnet" "main" {
   }
 }
 
-resource "aws_subnet" "main" {
+resource "aws_subnet" "example2" {
   vpc_id     = "${aws_vpc.main.id}"
   cidr_block = "10.0.1.0/24"
 
