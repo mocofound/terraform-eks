c3b47de7655f9e1cf951c49d3e76d1ced55f04b4
Alex Harness <9669770+mocofound@users.noreply.github.com>
2020-02-18 18:09:54 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ resource "aws_eks_node_group" "example" {
   cluster_name    = aws_eks_cluster.example.name
   node_group_name = "exampleNodeGroup"
   node_role_arn   = aws_iam_role.example.arn
-  subnet_ids      = aws_subnet.example[*].id
+  subnet_ids      = ["${aws_subnet.example1.id}", "${aws_subnet.example2.id}"]
 
   scaling_config {
     desired_size = 1
