50c873642b888208770c60227c18b3f775eb9eaf
Alex Harness <9669770+mocofound@users.noreply.github.com>
2020-02-18 19:39:28 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ resource "aws_vpc" "main3" {
 
 resource "aws_eks_node_group" "example" {
   cluster_name    = aws_eks_cluster.example.name
-  node_group_name = "exampleNodeGroup2"
+  node_group_name = "exampleNodeGroup4"
   node_role_arn   = aws_iam_role.examplenodegroup.arn
   subnet_ids      = ["${aws_subnet.example1.id}", "${aws_subnet.example2.id}"]
 
@@ -115,15 +115,15 @@ resource "aws_iam_role" "examplenodegroup" {
 
 resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.example.name
+  role       = aws_iam_role.examplenodegroup.name
 }
 
 resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.example.name
+  role       = aws_iam_role.examplenodegroup.name
 }
 
 resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.example.name
+  role       = aws_iam_role.examplenodegroup.name
 }
